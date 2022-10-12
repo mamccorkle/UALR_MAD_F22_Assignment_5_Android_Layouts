d0a38610c332dccc15eb205c4ae68ade6afb2613
Mark McCorkle <mamccorkle@ualr.edu>
2022-10-12 16:46:04 -0500

    Initial Commit - 202210121630
    Added Project template
    
    Added document signing
    
    Added README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# UALR - Mobile Application Development - F22 - Assignment 5 - Android Layout
+
+## CPSC 3367 Assignment 5 Topics:
+
+> Android Application Development - Using Android Layouts
+
+#### Instructions:
+
+:x::heavy_check_mark:  
+
+> :x: TODO 01. Define the layout for the MainActivity here.<br>
+> :x: TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money.<br>
+> :x: TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label.<br>
+
+#### Submission Due Date:
+
+>  Source Due: Monday, October 14, 2022 11:59 PM
---
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -0,0 +1,36 @@
+// https://github.com/UALR-MobileAppsDevelopmentCourse/assignment-05-mamccorkle
+//
+//  UALR - MAD - F22 - Assignment 5 - Android Layout
+//
+//  Project: UALR - Mobile Applications Development - Fall 2022 - Assignment 5 - Android Layout
+//  Created by: Mark McCorkle on 20221012
+//  Based on: Code Provided by Dr Ivan Rodriguez-Conde
+//
+//  IDE:
+//     Android Studio Chipmunk | 2021.2.1 Patch 2
+//     Build #AI-212.5712.43.2112.8815526, built on July 10, 2022
+//     Runtime version: 11.0.12+0-b1504.28-7817840 x86_64
+//     VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o.
+//     macOS 11.5.2
+//     GC: G1 Young Generation, G1 Old Generation
+//     Memory: 2048M
+//     Cores: 8
+//     Registry: external.system.auto.import.disabled=true
+//
+package com.ualr.layoutassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money
+
+    // TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
